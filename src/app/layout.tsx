3629e1de05140085b85dfdfd526b07c19a170b1a
rsarkarr <97837402+rsarkarr@users.noreply.github.com>
2024-08-09 03:16:24 +0530

    init frontend

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className} style={{
+          background: 'linear-gradient(to left, #323445, #12131c)',
+          color: "white",
+          minHeight: '100vh',
+          margin: 0,
+          }}>
+        <h1 className="flex justify-center items-center text-2xl font-bold mt-3">Your daily task manager.</h1>
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,138 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+import Link from "next/link";
+import React, { useState } from "react";
+
+const Signup = () => {
+  const [formData, setFormData] = useState({});
+  const [otpData, setOtpData]= useState({})
+  console.log(formData);
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  console.log(otpData);
+  
+
+  const handleOtpChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setOtpData({
+        ...otpData,
+        [e.target.id]: e.target.value
+      })
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("http://localhost:3333/api/v1/user/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+        credentials: "include",
+      });
+      const response = await res.json();
+      console.log(response);
+    } catch (error) {
+      return error;
+    }
+  };
+
+  const handleOtpVerification = async () => {
+   
+   try {
+     const res = await fetch("http://localhost:3333/api/v1/user/verify", {
+       method: "POST",
+       headers: {
+         "Content-Type": "application/json"
+       },
+       body: JSON.stringify(otpData),
+       credentials: "include"
+     })
+     const response = await res.json()
+     console.log(response);
+     
+   } catch (error) {
+    return error
+   }
+  }
+
+  return (
+    <div className="flex justify-center mt-4">
+      <form className="py-2" onSubmit={handleSubmit}>
+        <Input
+          placeholder="First name"
+          type="text"
+          id="firstName"
+          className="text-black"
+          onChange={handleOnChange}
+        ></Input>
+        <Input
+          placeholder="email"
+          id="email"
+          type="email"
+          className="mt-2 text-black"
+          onChange={handleOnChange}
+        ></Input>
+        <Input
+          placeholder="password"
+          id="password"
+          type="password"
+          className="mt-2 text-black"
+          onChange={handleOnChange}
+        ></Input>
+        <Button className="bg-blue-600 hover:bg-blue-800 mt-2 items-center">
+          <Dialog>
+            <DialogTrigger>Open</DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle className="text-black">
+                  Are you absolutely sure?
+                </DialogTitle>
+                <DialogDescription>
+                  <InputOTP maxLength={6}>
+                    <InputOTPGroup>
+                      <InputOTPSlot onChange={handleOtpChange} id="otp" index={0} />
+                      <InputOTPSlot onChange={handleOtpChange} id="otp" index={1} />
+                      <InputOTPSlot onChange={handleOtpChange} id="otp" index={2} />
+                    </InputOTPGroup>
+                    <InputOTPSeparator />
+                    <InputOTPGroup>
+                      <InputOTPSlot onChange={handleOtpChange} id="otp" index={3} />
+                      <InputOTPSlot onChange={handleOtpChange} id="otp" index={4} />
+                      <InputOTPSlot onChange={handleOtpChange} id="otp" index={5} />
+                    </InputOTPGroup>
+                  </InputOTP>
+                </DialogDescription>
+                 <Button onClick={handleOtpVerification}>Ok</Button>
+              </DialogHeader>
+            </DialogContent>
+           
+          </Dialog>
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
