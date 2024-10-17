d795e85d529e3a879fad5b60a48a2f1e44bb53e4
Raju Sarkar <97837402+rajusarkar23@users.noreply.github.com>
2024-10-17 11:26:44 +0000

    add signup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import Link from "next/link";
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      <Button className="bg-green-800">Shadcn button</Button>
+     <Link href={"/signup"}>Signup</Link>
     </main>
   );
 }
